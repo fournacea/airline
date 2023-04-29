b0bd7e10fa0e792bcae9b11f5378c6869568617c
Adam Fournace <fournacea@gmail.com>
2023-04-29 08:04:22 -0400

    ADD ability to book additional passengers on flight

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from .models import Flight
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Flight, Passenger
+
+
 
 
 # Create your views here.
@@ -17,8 +22,28 @@ def flight(request, flight_id):
     #Get the first flight matching the flight id
     flight = Flight.objects.get(pk=flight_id)
     
-    #Render page 
+    #Render page, provide variables to template
     return render(request, "flights/flight.html", {
         "flight": flight,
-        "passengers": flight.passengers.all()
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
+
+
+#Create view to book a flight
+def book(request, flight_id):
+
+    #Check if method=post
+    if request.method == "POST":
+        
+        #Get all flight objects
+        flight_obj = Flight.objects.get(pk=flight_id)
+
+        #Get all passenger objects
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+
+        #Add passenger to flight
+        passenger.flights.add(flight_obj)
+
+        #Redirect back to flight template
+        return HttpResponseRedirect(reverse("flight", args=(flight_obj.id,)))
