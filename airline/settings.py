164036741f374d8f3809ef3fbcba8b6a968435ac
Adam Fournace <fournacea@gmail.com>
2023-04-30 03:56:19 -0400

    ADD Bootstrap styling to flights/templates/index.html

--- airline/settings.py
+++ airline/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #My apps
     'flights',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'users/'
